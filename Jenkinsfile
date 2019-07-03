b053ebe2d3f1d83b25040d227438c54f77c8b418
SerdukovAA <seaa@1c.ru>
2019-07-03 14:30:15 +0300

    init updater

---
+++ Jenkinsfile
@@ -0,0 +1,59 @@
+pipeline {
+  agent any
+  stages {
+    stage('build') {
+      steps {
+        sh './gradlew clean bootJar'
+      }
+    }
+    stage('upload') {
+      steps {
+        sh '''HOST=vh112.timeweb.ru
+USER=cm41657_ftp
+PASS=FUP3qNjA
+
+PROJECT_VERSION=$(./gradlew properties | grep ^version: | awk \'{print $2}\')
+echo ${PROJECT_VERSION}
+
+FILE_HASH=$(sha1sum build/libs/updater-${PROJECT_VERSION}.jar | awk \'{print $1}\')
+
+cat >./index.php <<EOF
+<?php
+use \\Psr\\Http\\Message\\ServerRequestInterface as Request;
+use \\Psr\\Http\\Message\\ResponseInterface as Response;
+
+require \'../../vendor/autoload.php\';
+
+EOF
+
+echo \'$app = new \\Slim\\App;\' >> ./index.php
+echo \'$app ->get("/actual-version", function (Request $request, Response $response, array $args) {\' >> ./index.php
+echo \'$object = new stdClass();\' >> ./index.php
+echo \'$object->version = "\'${PROJECT_VERSION}\'";\' >> ./index.php
+echo \'$object->fileName = "updater-\'${PROJECT_VERSION}\'.jar";\' >> ./index.php
+echo \'$object->artifactLink = "http://nixvision.com/artifacts/updater/updater-\'${PROJECT_VERSION}\'.jar";\' >> ./index.php
+echo \'$object->artifactHash = "\'${FILE_HASH}\'";\' >> ./index.php
+echo \'return $response ->withStatus(200)->withHeader("Content-Type", "application/json")->write(json_encode($object));\' >> ./index.php
+
+echo \'});\' >> ./index.php
+echo \'$app ->run();\' >> ./index.php
+
+
+
+
+ftp -invp $HOST << EOF
+user $USER $PASS
+cd public_html
+mkdir artifacts
+cd artifacts
+mkdir updater
+cd updater
+put build/libs/updater-${PROJECT_VERSION}.jar updater-${PROJECT_VERSION}.jar
+put index.php index.php
+
+bye
+EOF'''
+      }
+    }
+  }
+}
---
+++ src/main/java/com/nixvision/updater/UpdaterApplication.java
@@ -0,0 +1,79 @@
+package com.nixvision.updater;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@SpringBootApplication
+public class UpdaterApplication {
+
+    private static String BACKUP_FILE_PATH =
+            System.getProperty("user.home") + File.separator + ".support-terminal" + File.separator +"backup"+ File.separator;
+
+
+
+    public static void main(String[] args) throws IOException {
+     try {
+         SpringApplication.run(UpdaterApplication.class, args);
+
+         log.info("Updater запущен. Получены следующие параметры запуска");
+
+         String newAppFilePath = args[0];
+         log.info("newAppFilePath " + newAppFilePath);
+         String currentApplicationRoot = args[1];
+         log.info("currentApplicationRoot " + currentApplicationRoot);
+         String oldJarPath = args[2];
+         log.info("oldJarPath " + oldJarPath);
+
+
+         createPathWithParentsOrClear(BACKUP_FILE_PATH);
+         log.info("Чистим папку бекапа - Успешно");
+
+
+         File oldJar = new File(oldJarPath);
+         File newJar = new File(newAppFilePath);
+
+
+         FileUtils.copyFileToDirectory(oldJar, new File(BACKUP_FILE_PATH));
+         log.info("Бекап - Успешно");
+
+
+         FileUtils.forceDelete(oldJar);
+         log.info("Удаляем старый jar - Успешно");
+
+         FileUtils.copyFileToDirectory(newJar, new File(currentApplicationRoot));
+         log.info("Копия нового jar - Успешно");
+
+
+         StringBuilder commandBuilder = new StringBuilder();
+         commandBuilder.append("java -jar").append(" ")
+                 //запускаем апдейтер
+                 .append(currentApplicationRoot).append(File.separator)
+                 //передаем новый файл
+                 .append(newJar.getName());
+
+         log.info("Запуск нового jar - " + commandBuilder.toString());
+         Process proc = Runtime.getRuntime().exec(commandBuilder.toString());
+
+         log.info("Выпиливаемся");
+         System.exit(0);
+     }catch (Exception ex){
+         log.error("Ошибка обновления ", ex);
+     }
+
+    }
+
+    private static File createPathWithParentsOrClear(String path) throws IOException {
+        File file = new File(path);
+        file.getParentFile().mkdirs();
+        file.mkdir();
+        FileUtils.cleanDirectory(file);
+        return file;
+    }
+
+}
